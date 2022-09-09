78d505cc1598369637e1fd8f53e8ef4885d60522
Yehor Orlov <egor7orlov@gmail.com>
2022-09-09 22:05:33 +0300

    CORS added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
+app.options('*', cors())
 app.use(express.static('public'));
 app.listen(process.env.PORT || 3000);
